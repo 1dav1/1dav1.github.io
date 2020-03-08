c80b47bf724e862a0b74dde6cb5c46066087ce02
1dav1 <51138383+1dav1@users.noreply.github.com>
2020-03-08 22:33:27 +0300

    Update moveScript.js

--- move-js/script/moveScript.js
+++ move-js/script/moveScript.js
@@ -4,10 +4,7 @@ let div = document.getElementById('movable1');
 let div2 = document.getElementById('movable2')
 let circ1 = document.getElementById('circuit');
 let circ2 = document.getElementById('circuit2');
-butt.addEventListener('click', () =>{
-    div.style.left = '50px';
-    div2.style.left = '0.1px';
-})
+
 circ1.addEventListener('click', () => {
     if (div.style.left == '50px'){
         div2.style.left = '-20px';
@@ -16,8 +13,8 @@ circ1.addEventListener('click', () => {
     }
 })
 circ2.addEventListener('click', () => {
-    if (div2.style.left == '-20px'){
+    if (div2.offsetLeft == -18){
         div.style.left = '50px';
-        div2.style.left = '0.1px';
+        div2.style.left = '0px';
     }
 })
